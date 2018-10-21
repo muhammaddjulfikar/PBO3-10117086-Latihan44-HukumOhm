7447c02fae48af8d0d2e3dfb8a779d01107d3844
djulfikar <muhammaddjulfikar7@gmail.com>
2018-10-21 16:24:12 +0700

    PBO3-10117086-Latihan44-HukumOhm

---
+++ src/pbo3/pkg10117086/latihan44/hukumohm/Baterai.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan44.hukumohm;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Baterai {
+    private double kuatArus = 3.0, hambatan = 12.0;
+    
+    
+    public Baterai(){
+        
+    }
+    public double getKuatArus() {
+        System.out.println("kuat Arus : "+kuatArus+" Ampere ");
+        return kuatArus;
+    }
+
+    public double getHambatan() {
+        System.out.println("Hambatan : "+hambatan+" ohm ");
+        return hambatan;
+    }
+    
+    public double hitungTegangan(){
+        return kuatArus*hambatan;
+    }
+    
+}
---
+++ src/pbo3/pkg10117086/latihan44/hukumohm/PBO310117086Latihan44HukumOhm.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan44.hukumohm;
+
+/**
+ *
+ * @author Lenovo 
+ * NAMA : RD Muhammad Djulfikar BU 
+ * KELAS : IF3 
+ * NIM : 10117086
+ * DESKRIPSI PROGRAM :
+ */
+public class PBO310117086Latihan44HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Baterai objB = new Baterai();
+        System.out.println("=====Hukum Ohm=====");
+        System.out.println("Kuat arus yang mengalir pada suatu kawat penghantar"
+                + "akan berbanding lurus dengan beda potensial pada ujung ujung"
+                + "kawat penghantar tersebut asalkan suhu kawat dijaga konstan."
+                + "");
+
+        objB.getKuatArus();
+        objB.getHambatan();
+        System.out.println("Hasil Tegangan : " + objB.hitungTegangan() +
+                " volt ");
+
+    }
+
+}
